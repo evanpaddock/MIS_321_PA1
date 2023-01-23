cf7dcbca38b8d897e558695d13d9fec32a6888a2
Evan <112584442+evanpaddock@users.noreply.github.com>
2023-01-22 21:41:30 -0600

    up to date

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] list = {"1","2","3","4"};
+Menu.Main(list);
--- menu.cs
+++ menu.cs
@@ -1,21 +1,117 @@
 namespace MIS_321_PA1
 {
-    public class menu
+    public class Menu
     {
+        public static List<Option> options{get; set;}
         public static string AskUserSelection()
         {
+            
+            
             string userSelection;
 
             do
             {
-                System.Console.WriteLine("Write 1 for");
-                System.Console.WriteLine("Write 1 for");
-                System.Console.WriteLine("Write 1 for");
-                System.Console.WriteLine("Write 1 for");
-                System.Console.WriteLine("Write 1 for");
+                System.Console.WriteLine("Type 1 for");
+                System.Console.WriteLine("Type 1 for");
+                System.Console.WriteLine("Type 1 for");
+                System.Console.WriteLine("Type 1 for");
+                System.Console.WriteLine("Type 1 for");
                 userSelection = Console.ReadLine();
             }while(userSelection != "1");
             return userSelection;
+        }        
+        static void Main(string[] args)
+        {
+            // Create options that you want your menu to have
+            options = new List<Option>
+            {
+                new Option("Thing", () => WriteTemporaryMessage("Hi")),
+                new Option("Another Thing", () =>  WriteTemporaryMessage("How Are You")),
+                new Option("Yet Another Thing", () =>  WriteTemporaryMessage("Today")),
+                new Option("Exit", () => Environment.Exit(0)),
+            };
+
+            // Set the default index of the selected item to be the first
+            int index = 0;
+
+            // Write the menu out
+            WriteMenu(options, options[index]);
+
+            // Store key info in here
+            ConsoleKeyInfo keyinfo;
+            do
+            {
+                keyinfo = Console.ReadKey();
+
+                // Handle each key input (down arrow will write the menu again with a different selected item)
+                if (keyinfo.Key == ConsoleKey.DownArrow)
+                {
+                    if (index + 1 < options.Count)
+                    {
+                        index++;
+                        WriteMenu(options, options[index]);
+                    }
+                }
+                if (keyinfo.Key == ConsoleKey.UpArrow)
+                {
+                    if (index - 1 >= 0)
+                    {
+                        index--;
+                        WriteMenu(options, options[index]);
+                    }
+                }
+                // Handle different action for the option
+                if (keyinfo.Key == ConsoleKey.Enter)
+                {
+                    options[index].Selected.Invoke();
+                    index = 0;
+                }
+            }
+            while (keyinfo.Key != ConsoleKey.X);
+
+            Console.ReadKey();
+
+        }
+        // Default action of all the options. You can create more methods
+        static void WriteTemporaryMessage(string message)
+        {
+            Console.Clear();
+            Console.WriteLine(message);
+            Thread.Sleep(3000);
+            WriteMenu(options, options.First());
+        }
+
+
+
+        static void WriteMenu(List<Option> options, Option selectedOption)
+        {
+            Console.Clear();
+
+            foreach (Option option in options)
+            {
+                if (option == selectedOption)
+                {
+                    Console.Write("> ");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine(option.Name);
+            }
+        }
+    }
+
+    public class Option
+    {
+        public string Name { get; }
+        public Action Selected { get; }
+
+        public Option(string name, Action selected)
+        {
+            Name = name;
+            Selected = selected;
         }
     }
 }
