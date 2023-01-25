3064d495b3daf6f51ecd9b5c78120770cd3ba009
Evan <112584442+evanpaddock@users.noreply.github.com>
2023-01-24 20:02:24 -0600

    try/Catch

--- FileHandlin.cs
+++ FileHandlin.cs
@@ -3,8 +3,18 @@ public class FileHandlin
 {
     static public void ReadFromJSON()
     {
-        string json = File.ReadAllText("songs.json");
-        Song.allsongs = JsonConvert.DeserializeObject<List<Song>>(json);
+        try{
+            string json = File.ReadAllText("songs.json");
+            if (json == "")
+            {
+                json = null;
+            }
+            Song.allsongs = JsonConvert.DeserializeObject<List<Song>>(json);
+        }catch(Exception e){
+            System.Console.WriteLine("No songs found. Please create new song to continue.");
+            Song.AddNew();
+        }
+        
     }
     static public void WriteOutJSON()
     {
